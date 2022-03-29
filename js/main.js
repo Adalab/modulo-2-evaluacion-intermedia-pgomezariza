0f005514ab16f2615893fc37a437b4621f3754fd
Paula Gomez Ariza <pgomezariza@gmail.com>
2022-03-29 11:22:09 +0200

    try to fix js

--- js/main.js
+++ js/main.js
@@ -8,17 +8,36 @@ const apuesta = document.querySelector('.js_apuesta');
 const js_text1 = document.querySelector('.js_text1');
 const btn = document.querySelector('.js_btn');
 
+//const same = win;
+//const different = lost;
+
 
 //Declarar funciones y eventos
 
+    //crear numero aleatorio
 function getRandomNumber() {
-  return Math.ceil(Math.random() * 6);
+  const numAleat = parseInt(Math.random()*6);
+  return numAleat;
+}
+
+function cambiarElMensaje(){
+  //Generar el numero aleatorio
+  //const numAleat = parseInt(Math.random()*6);
+  const numAleat = getRandomNumber(6);
+
+  if(numAleat === ){
+    //Same
+
+  }
+  else {
+    //Different
+
+  }
 }
 
 
-
-
 //Codigo a ejecutar cuando cargo la página
+
 btn.addEventListener("click", () => {
-  play(Jugar);
+  
 });
