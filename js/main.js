a83209794279031365b3e8828be5d8bde309d896
Paula Gomez Ariza <pgomezariza@gmail.com>
2022-03-29 10:32:29 +0200

    add sth js

--- js/main.js
+++ js/main.js
@@ -2,9 +2,23 @@
 
 //Elementos que necesito del HTML 
 
-const btn = document.querySelector('.js_btn');
+
 const select = document.querySelector('.js_select');
+const apuesta = document.querySelector('.js_apuesta');
 const js_text1 = document.querySelector('.js_text1');
+const btn = document.querySelector('.js_btn');
+
 
 //Declarar funciones y eventos
 
+function getRandomNumber() {
+  return Math.ceil(Math.random() * 6);
+}
+
+
+
+
+//Codigo a ejecutar cuando cargo la página
+btn.addEventListener("click", () => {
+  play(Jugar);
+});
