9cd16ddd005a73ed9ac077aaab6481c024e66b68
Paula Gomez Ariza <pgomezariza@gmail.com>
2022-03-29 09:42:00 +0200

    add html

---
+++ js/main.js
@@ -0,0 +1,10 @@
+'use strict'
+
+//Elementos que necesito del HTML 
+
+const btn = document.querySelector('.js_btn');
+const select = document.querySelector('.js_select');
+const js_text1 = document.querySelector('.js_text1');
+
+//Declarar funciones y eventos
+
